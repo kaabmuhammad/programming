ebb6bc3b285c2d33597ecb40f40de0a3c521a5b4
AzureAD\MuhammadKaab <muhammad.kaab@riversafe.co.uk>
2022-11-02 20:41:16 +0000

    Updated tempalates

--- main.py
+++ main.py
@@ -5,25 +5,24 @@ from roman_numeral_converter import *
 app: Flask = Flask(__name__)
 
 
-values = [{}]
+cal = []
 
 
 @app.route("/")
 def index():
-    return render_template("index.html", message=values)
+    return render_template("index.html", message=cal)
 
 
 @app.route("/home/", methods=("GET", "POST"))
 def create():
     if request.method == "POST":
         num = int(request.form["number"])
-        cal = []
 
-        cal.append({num: to_roman_numeral(num)})
+        cal.insert(0, to_roman_numeral(num))
         string = request.form["roman"]
 
-        cal.append({string: to_arabic_number(string)})
-        values.insert(1, cal)
+        cal.insert(1, to_arabic_number(string))
+
         return redirect(url_for("index"))
 
     return render_template("home.html")
